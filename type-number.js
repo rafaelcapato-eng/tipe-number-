440272de56573f7133b115707dfcbfa17dbc033e
rafaelcapato-eng <rafael.capato@escola.pr.gov.br>
2026-03-02 15:23:42 +0000

    operaçao matematica e terminal

--- type-number.js
+++ type-number.js
@@ -1,4 +1,7 @@
-// tipo number
+// type number
 const meuNumero = 3;
 const primeiroNumero = 1;
 const segundoNumero = 2;
+const operacaoMatematica = primeiroNumero + segundoNumero
+
+console.log(operacaoMatematica)

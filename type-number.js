0fa7eaa3db6d01bd0bc11bd63cf6fa90d65e0271
rafaelcapato-eng <rafael.capato@escola.pr.gov.br>
2026-03-03 15:25:13 +0000

    .

--- type-number.js
+++ type-number.js
@@ -2,6 +2,11 @@
 const meuNumero = 3;
 const primeiroNumero = 1;
 const segundoNumero = 2;
-const operacaoMatematica = primeiroNumero + segundoNumero
+const operacaoMatematica = primeiroNumero / segundoNumero
 
 console.log(operacaoMatematica)
+
+// ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemzaro =.5;
